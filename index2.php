6d5f231c31820736fb7693103fcc07851bce9813
DarkGamer1116 <62205784+DarkGamer1116@users.noreply.github.com>
2020-04-19 19:23:32 +0200

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,67 @@
+<?php
+echo "bl"."be"."cku";
+echo "<br>";
+echo "mat" . "ka";
+echo "<br>";
+echo 5 . 7;
+echo 2.3;
+
+$a = 2;
+$b = 3;
+
+print "<br> ";
+echo $a += $b;
+var_dump($a+=$b);
+
+print "<br> ";
+echo $a--;
+var_dump($a--);
+
+print "<br> ";
+echo $a++;
+var_dump($a++);
+
+print "<br>";
+echo $a -= $b;
+var_dump($a-=$b);
+
+print "<br>";
+echo $a *= $b;
+var_dump($a*=$b);
+
+print "<br>";
+echo $a /= $b;
+var_dump($a/=$b);
+
+print "<br>";
+var_dump($a>$b);
+
+print "<br>";
+var_dump($a!=$b);
+
+print "<br>";
+echo $a==$b;
+var_dump($a==$b);
+
+print "<br>";
+var_dump($a<$b);
+
+print "<br>";
+echo $a*$b;
+var_dump($a*$b);
+
+print "<br>";
+echo $a/$b;
+var_dump($a/$b);
+
+print "<br>";
+echo $a+$b;
+var_dump($a+$b);
+
+print "<br>";
+echo $a-$b;
+var_dump($a-$b);
+    
+?>
+
+</body>
